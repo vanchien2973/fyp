52266b6ba0ae8874c566d75200edde7147b0a6a0
vanchien2973 <129261417+vanchien2973@users.noreply.github.com>
2024-08-18 22:08:25 +0700

    custom ui

--- client/app/components/Admin/courses/CourseInformation.js
+++ client/app/components/Admin/courses/CourseInformation.js
@@ -9,98 +9,145 @@ const CourseInformation = ({ courseInfor, setCourseInfor, active, setActive }) =
     setActive(active + 1);
   };
 
-  return (
-    <div className="w-[80%] m-auto mt-24 dark:bg-dark-900">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-6">
-          <label className="block text-sm font-medium mb-2" htmlFor="">Course Name</label>
-          <input
-            type="text"
-            id="name"
-            value={courseInfor.name}
-            onChange={(e) => setCourseInfor({ ...courseInfor, name: e.target.value })}
-            className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
-          />
-        </div>
+  const handleChangeFile = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (reader.readyState === 2) {
+          setCourseInfor({ ...courseInfor, thumbnail: reader.result });
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-        <div className="mb-6">
-          <label className="block text-sm font-medium mb-2" htmlFor="">Course Description</label>
-          <textarea
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  }
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDragging(false);
+  }
+
+  const handleDragDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+    const file = e.dataTransfer.file[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setCourseInfor({ ...courseInfor, thumbnail: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+return (
+  <div className="w-[80%] m-auto mt-24 dark:bg-dark-900">
+    <form onSubmit={handleSubmit}>
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2" htmlFor="name">Course Name</label>
+        <input
           type="text"
+          id="name"
+          value={courseInfor.name}
+          onChange={(e) => setCourseInfor({ ...courseInfor, name: e.target.value })}
+          className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
+        />
+      </div>
+
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2" htmlFor="description">Course Description</label>
+        <textarea
+          type="text"
+          id="description"
+          value={courseInfor.description}
+          onChange={(e) => setCourseInfor({ ...courseInfor, description: e.target.value })}
+          className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
+          rows="4"
+        />
+      </div>
+
+      <div className="mb-6 flex justify-between">
+        <div className="w-[48%]">
+          <label className="block text-sm font-medium mb-2" htmlFor="price">Price</label>
+          <input
+            type="number"
             id="price"
-            value={courseInfor.description}
-            onChange={(e) => setCourseInfor({ ...courseInfor, c: e.target.value })}
-            className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
-            rows="4"
-          />
-        </div>
-
-        <div className="mb-6 flex justify-between">
-          <div className="w-[48%]">
-            <label className="block text-sm font-medium mb-2" htmlFor="">Price</label>
-            <input
-              type="number"
-              id="price"
-              value={courseInfor.price}
-              onChange={(e) => setCourseInfor({ ...courseInfor, price: e.target.value })}
-              className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
-            />
-          </div>
-
-          <div className="w-[48%]">
-            <label className="block text-sm font-medium mb-2" htmlFor="">Estimated Price (optional)</label>
-            <input
-              type="number"
-              id="estimatedPrice"
-              value={courseInfor.estimatedPrice}
-              onChange={(e) => setCourseInfor({ ...courseInfor, estimatedPrice: e.target.value })}
-              className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
-            />
-          </div>
-        </div>
-
-        <div className="mb-6">
-          <label className="block text-sm font-medium mb-2" htmlFor="">Course Tags</label>
-          <input
-            type="text"
-            id="tags"
-            value={courseInfor.tags}
-            onChange={(e) => setCourseInfor({ ...courseInfor, tags: e.target.value })}
+            value={courseInfor.price}
+            onChange={(e) => setCourseInfor({ ...courseInfor, price: e.target.value })}
             className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
           />
         </div>
 
-        <div className="mb-6">
-          <label className="block text-sm font-medium mb-2" htmlFor="">Course Level</label>
+        <div className="w-[48%]">
+          <label className="block text-sm font-medium mb-2" htmlFor="estimatedPrice">Estimated Price (optional)</label>
           <input
-            type="text"
-            id="level"
-            value={courseInfor.level}
-            onChange={(e) => setCourseInfor({ ...courseInfor, level: e.target.value })}
+            type="number"
+            id="estimatedPrice"
+            value={courseInfor.estimatedPrice}
+            onChange={(e) => setCourseInfor({ ...courseInfor, estimatedPrice: e.target.value })}
             className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
           />
         </div>
+      </div>
 
-        <div className="mb-6">
-          <label className="block text-sm font-medium mb-2" htmlFor="">Demo URL</label>
-          <input
-            type="text"
-            id="demoUrl"
-            value={courseInfor.demoUrl}
-            onChange={(e) => setCourseInfor({ ...courseInfor, demoUrl: e.target.value })}
-            className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent text-white"
-          />
-        </div>
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2" htmlFor="tags">Course Tags</label>
+        <input
+          type="text"
+          id="tags"
+          value={courseInfor.tags}
+          onChange={(e) => setCourseInfor({ ...courseInfor, tags: e.target.value })}
+          className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
+        />
+      </div>
 
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600"
+      <div className="mb-6">
+        <label className="block text-sm font-medium mb-2" htmlFor="level">Course Level</label>
+        <input
+          type="text"
+          id="level"
+          value={courseInfor.level}
+          onChange={(e) => setCourseInfor({ ...courseInfor, level: e.target.value })}
+          className="w-full p-2 border border-[#343a46] dark:border-black rounded dark:bg-transparent dark:border-dark-700  bg-transparent"
+        />
+      </div>
+
+      <div className="mb-6">
+        <input
+          type="file"
+          accept="image/*"
+          id="file"
+          className="hidden"
+          onChange={handleChangeFile}
+        />
+        <label
+          htmlFor="file"
+          className={`w-full min-h-[10vh] p-3 flex items-center justify-center border border-[#343a46] dark:border-black rounded dark:border-dark-700 ${dragging ? "bg-blue-500" : "bg-transparent"
+            }`}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDragDrop}
         >
-          Next
-        </button>
-      </form>
-    </div>
-  );
-};
+          {courseInfor.thumbnail ? (
+            <img
+              src={courseInfor.thumbnail}
+              alt=""
+              className="max-h-full w-full object-cover"
+            />
+          ) : (
+            <span>
+              Drag and drop your thumbnail here or click to browse
+            </span>
+          )}
+        </label>
+      </div>
+    </form>
+  </div>
+)};
 
 export default CourseInformation;
--- client/app/utils/ThemeAdmin.js
+++ client/app/utils/ThemeAdmin.js
@@ -155,7 +155,7 @@ export const themeSettings = (mode) => {
                     light: colors.grey[100],
                 },
                 background: {
-                    default: colors.primary[900],
+                    default: colors.primary[400],
                 }
             })
         },
